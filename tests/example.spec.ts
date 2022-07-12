1894a0a81242f92d5803e912f361686e4e2fc3ad
Denis Kolovorotnyi <denis.kolovorotnyi@nuorder.com>
2022-07-12 23:04:13 +0200

    added 1 more test

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -6,9 +6,9 @@ test.beforeEach(async ({page}) => {
 });
 
 test.describe('Log in with incorrect data', () => {
-    test('should allow me to add todo items', async ({page}) => {
+    test('user should see error message', async ({page}) => {
         allure.severity("critical")
-        allure.owner("RapDog")
+        allure.owner("RapDog64")
         allure.link({url: 'http://demowebshop.tricentis.com/', name: 'Staging'})
         allure.issue({url: 'Jira', name: "Bug #1"})
         allure.feature("Authentication feature")
@@ -27,6 +27,37 @@ test.describe('Log in with incorrect data', () => {
             await page.locator('#Password').fill(password);
         });
 
+        await test.step("Click Log in button", async () => {
+            await page.locator("//input[@value='Log in']").click();
+        });
+
+        await test.step("Verify error message is displayed", async () => {
+            await expect(page.locator('div[class="validation-summary-errors"]')).toHaveText(unsuccessfulMessage);
+        });
+
+    });
+
+
+    test('user should see error message when email is incorrect', async ({page}) => {
+        allure.severity("critical")
+        allure.owner("RapDog64")
+        allure.link({url: 'http://demowebshop.tricentis.com/', name: 'Staging'})
+        allure.issue({url: 'Jira', name: "Bug #1"})
+        allure.feature("Authentication feature")
+        allure.story("Log in as a user")
+
+
+        const unsuccessfulMessage = "Login was unsuccessful. Please correct the errors and try again. No customer account found";
+        const email = "test-email";
+        const password = "test-password";
+
+        await test.step(`Type email:${email}`, async () => {
+            await page.locator('#Email').fill(email);
+        });
+
+        await test.step(`Type password:${password}`, async () => {
+            await page.locator('#Password').fill(password);
+        });
 
         await test.step("Click Log in button", async () => {
             await page.locator("//input[@value='Log in']").click();
