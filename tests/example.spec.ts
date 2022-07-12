b38d7ea18237c1d01bb25c2e4242800907f76edb
Denis Kolovorotnyi <denis.kolovorotnyi@nuorder.com>
2022-07-12 22:51:08 +0200

    added allure report

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -1,18 +1,40 @@
-import { test, expect, type Page } from '@playwright/test';
+import {expect, type Page, test} from '@playwright/test';
+import {allure} from "allure-playwright"
 
-test.beforeEach(async ({ page }) => {
-  await page.goto("http://demowebshop.tricentis.com/login");
+test.beforeEach(async ({page}) => {
+    await page.goto("http://demowebshop.tricentis.com/login");
 });
 
 test.describe('Log in with incorrect data', () => {
-  test('should allow me to add todo items', async ({ page }) => {
-    const unsuccessfulMessage = "Login was unsuccessful. Please correct the errors and try again. No customer account found";
+    test('should allow me to add todo items', async ({page}) => {
+        allure.severity("critical")
+        allure.owner("RapDog")
+        allure.link({url: 'http://demowebshop.tricentis.com/', name: 'Staging'})
+        allure.issue({url: 'Jira', name: "Bug #1"})
+        allure.feature("Authentication feature")
+        allure.story("Log in as a user")
 
 
-    await page.locator('#Email').fill("dsadsad@mail.ru");
-    await page.locator('#Password').fill("dsadsadsad");
-    await page.locator("//input[@value='Log in']").click();
+        const unsuccessfulMessage = "Login was unsuccessful. Please correct the errors and try again. No customer account found";
+        const email = "dsadsad@mail.ru";
+        const password = "dsadsadsad";
 
-    await expect(page.locator('div[class="validation-summary-errors"]')).toHaveText(unsuccessfulMessage);
-  });
+        await test.step(`Type email:${email}`, async () => {
+            await page.locator('#Email').fill(email);
+        });
+
+        await test.step(`Type password:${password}`, async () => {
+            await page.locator('#Password').fill(password);
+        });
+
+
+        await test.step("Click Log in button", async () => {
+            await page.locator("//input[@value='Log in']").click();
+        });
+
+        await test.step("Verify error message is displayed", async () => {
+            await expect(page.locator('div[class="validation-summary-errors"]')).toHaveText(unsuccessfulMessage);
+        });
+
+    });
 });
